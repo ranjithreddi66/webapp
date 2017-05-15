f15848af2ba344a41ee69921a6707b02991af20f
Miguel Pignatelli <emepyc@gmail.com>
2017-05-15 17:52:34 +0100

    all in, but iv

--- app/plugins/interactions-viewer.js
+++ app/plugins/interactions-viewer.js
@@ -21,11 +21,11 @@ angular.module('plugins')
 
         return {
             restrict: 'E',
-            template: '<!-- hint --><div>Interaction viewer here</div>',
+            template: '<!-- hint --><div>Interaction viewer here</div>' +
             // '<div>' +
             // '    <p class="cttv-section-intro">Summary of interactions for {{target.approved_symbol}} based on <a target=_blank href="http://omnipathdb.org/">OmniPath DB</a> data. When 2 targets are selected details on the interaction are shown.</p>' +
             // '</div>' +
-            // '<interactors-star-plot interactors="interactors" categories="categories" selected="target.approved_symbol"></interactors-star-plot>',
+            '<interactors-star-plot interactors="interactors" categories="categories" selected="target.approved_symbol"></interactors-star-plot>',
             scope: {
                 target: '=',
                 width: '='
