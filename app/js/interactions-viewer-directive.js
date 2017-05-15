4a6f58a629bbf101da105214e634b926b01d2189
Miguel Pignatelli <emepyc@gmail.com>
2017-05-15 16:18:41 +0100

    1 more commit

--- app/js/interactions-viewer-directive.js
+++ app/js/interactions-viewer-directive.js
@@ -238,6 +238,7 @@ angular.module('cttvDirectives')
                 $timeout(function () {
                     scope.showSpinner = false;
                     console.log('here we should run iv...');
+                    
                     // iv(document.getElementById("interactionsViewerMultipleTargets"));
                 }, 0);
 
